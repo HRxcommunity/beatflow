00c2d4b20330629f4307dc2c8ecc9cd8d6cdf87f
HRxCommunity <hrxcommunity@kali>
2026-06-30 23:05:21 +0530

    Track generated Hive files

---
+++ lib/data/models/settings_model.g.dart
@@ -0,0 +1,103 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SettingsModelAdapter extends TypeAdapter<SettingsModel> {
+  @override
+  final int typeId = 2;
+
+  @override
+  SettingsModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SettingsModel()
+      ..themeMode = fields[0] as int
+      ..accentColorIndex = fields[1] as int
+      ..eqEnabled = fields[2] as bool
+      ..eqBands = (fields[3] as List).cast<double>()
+      ..eqPreset = fields[4] as String
+      ..bassBoost = fields[5] as double
+      ..virtualizer = fields[6] as double
+      ..reverb = fields[7] as double
+      ..loudnessEnhancer = fields[8] as double
+      ..filterShortClips = fields[9] as bool
+      ..excludedFolders = (fields[10] as List).cast<String>()
+      ..showAlbumArtInNotification = fields[11] as bool
+      ..gaplessPlayback = fields[12] as bool
+      ..backgroundType = fields[13] as int
+      ..backgroundImagePath = fields[14] as String?
+      ..backgroundDimOpacity = fields[15] as double
+      ..songCardOpacity = fields[16] as double
+      ..songCardColorIndex = fields[17] as int
+      ..songCardColorValue = fields[18] as int
+      ..togetherBgType = fields[19] as int
+      ..togetherBgImagePath = fields[20] as String?
+      ..togetherBgDimOpacity = fields[21] as double;
+  }
+
+  @override
+  void write(BinaryWriter writer, SettingsModel obj) {
+    writer
+      ..writeByte(22)
+      ..writeByte(0)
+      ..write(obj.themeMode)
+      ..writeByte(1)
+      ..write(obj.accentColorIndex)
+      ..writeByte(2)
+      ..write(obj.eqEnabled)
+      ..writeByte(3)
+      ..write(obj.eqBands)
+      ..writeByte(4)
+      ..write(obj.eqPreset)
+      ..writeByte(5)
+      ..write(obj.bassBoost)
+      ..writeByte(6)
+      ..write(obj.virtualizer)
+      ..writeByte(7)
+      ..write(obj.reverb)
+      ..writeByte(8)
+      ..write(obj.loudnessEnhancer)
+      ..writeByte(9)
+      ..write(obj.filterShortClips)
+      ..writeByte(10)
+      ..write(obj.excludedFolders)
+      ..writeByte(11)
+      ..write(obj.showAlbumArtInNotification)
+      ..writeByte(12)
+      ..write(obj.gaplessPlayback)
+      ..writeByte(13)
+      ..write(obj.backgroundType)
+      ..writeByte(14)
+      ..write(obj.backgroundImagePath)
+      ..writeByte(15)
+      ..write(obj.backgroundDimOpacity)
+      ..writeByte(16)
+      ..write(obj.songCardOpacity)
+      ..writeByte(17)
+      ..write(obj.songCardColorIndex)
+      ..writeByte(18)
+      ..write(obj.songCardColorValue)
+      ..writeByte(19)
+      ..write(obj.togetherBgType)
+      ..writeByte(20)
+      ..write(obj.togetherBgImagePath)
+      ..writeByte(21)
+      ..write(obj.togetherBgDimOpacity);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SettingsModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
---
+++ lib/data/models/song_model.g.dart
@@ -0,0 +1,73 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'song_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class SongModelAdapter extends TypeAdapter<SongModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  SongModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return SongModel()
+      ..id = fields[0] as int
+      ..title = fields[1] as String
+      ..artist = fields[2] as String
+      ..album = fields[3] as String
+      ..albumArtist = fields[4] as String
+      ..duration = fields[5] as int
+      ..data = fields[6] as String
+      ..folderPath = fields[7] as String?
+      ..dateAdded = fields[8] as DateTime?
+      ..playCount = fields[9] as int
+      ..isFavorite = fields[10] as bool
+      ..lastPlayed = fields[11] as DateTime?;
+  }
+
+  @override
+  void write(BinaryWriter writer, SongModel obj) {
+    writer
+      ..writeByte(12)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.artist)
+      ..writeByte(3)
+      ..write(obj.album)
+      ..writeByte(4)
+      ..write(obj.albumArtist)
+      ..writeByte(5)
+      ..write(obj.duration)
+      ..writeByte(6)
+      ..write(obj.data)
+      ..writeByte(7)
+      ..write(obj.folderPath)
+      ..writeByte(8)
+      ..write(obj.dateAdded)
+      ..writeByte(9)
+      ..write(obj.playCount)
+      ..writeByte(10)
+      ..write(obj.isFavorite)
+      ..writeByte(11)
+      ..write(obj.lastPlayed);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SongModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
